8e7387f7feb0a3d25bc539b2733a7c7d2961aac2
Robbert Brandsma <i@robbertbrandsma.nl>
2025-04-05 12:27:21 +0200

    Remove edge agent and proto from the package

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.1
 import PackageDescription
 
 let package = Package(
@@ -7,19 +7,18 @@ let package = Package(
         .macOS(.v15)
     ],
     products: [
-        .executable(name: "edge", targets: ["edge"]),
-        .executable(name: "edge-agent", targets: ["edge-agent"]),
+        .executable(name: "edge", targets: ["edge"])
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.6.3"),
-        .package(url: "https://github.com/grpc/grpc-swift.git", from: "2.0.0"),
         .package(url: "https://github.com/grpc/grpc-swift-nio-transport.git", from: "1.0.0"),
-        .package(url: "https://github.com/grpc/grpc-swift-protobuf.git", from: "1.0.0"),
-        .package(url: "https://github.com/swift-server/swift-service-lifecycle.git", from: "2.7.0"),
-        .package(url: "https://github.com/grpc/grpc-swift-extras.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.81.0"),
         .package(url: "https://github.com/apple/swift-crypto.git", from: "3.12.2"),
+        .package(
+            url: "https://github.com/apache-edge/edge-agent-common.git",
+            revision: "952035635c630d366dfbcff04af1934c7c051f23"
+        ),
     ],
     targets: [
         /// The main executable provided by edge-cli.
@@ -30,28 +29,11 @@ let package = Package(
                 .product(name: "Logging", package: "swift-log"),
                 .product(name: "_NIOFileSystem", package: "swift-nio"),
                 .product(name: "GRPCNIOTransportHTTP2", package: "grpc-swift-nio-transport"),
-                .target(name: "EdgeAgentGRPC"),
+                .product(name: "EdgeAgentGRPC", package: "edge-agent-common"),
                 .target(name: "EdgeCLI"),
             ],
             resources: [
                 .copy("Resources")
-            ]
-        ),
-
-        /// The EdgeAgent executable. It's currently here for development purposes, and will be
-        /// moved to a separate package in the future.
-        .executableTarget(
-            name: "edge-agent",
-            dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "Logging", package: "swift-log"),
-                .product(name: "GRPCNIOTransportHTTP2", package: "grpc-swift-nio-transport"),
-                .product(name: "ServiceLifecycle", package: "swift-service-lifecycle"),
-                .product(name: "GRPCServiceLifecycle", package: "grpc-swift-extras"),
-                .product(name: "GRPCHealthService", package: "grpc-swift-extras"),
-                .product(name: "_NIOFileSystem", package: "swift-nio"),
-                .target(name: "EdgeAgentGRPC"),
-                .target(name: "Shell"),
             ]
         ),
 
@@ -60,7 +42,7 @@ let package = Package(
             name: "EdgeCLI",
             dependencies: [
                 .target(name: "ContainerBuilder"),
-                .target(name: "Shell"),
+                .product(name: "Shell", package: "edge-agent-common"),
                 .product(name: "Logging", package: "swift-log"),
             ]
         ),
@@ -69,28 +51,8 @@ let package = Package(
         .target(
             name: "ContainerBuilder",
             dependencies: [
-                .target(name: "Shell"),
+                .product(name: "Shell", package: "edge-agent-common"),
                 .product(name: "Crypto", package: "swift-crypto"),
-            ]
-        ),
-
-        /// Utility for executing shell commands.
-        .target(
-            name: "Shell",
-            dependencies: [
-                .product(name: "Logging", package: "swift-log")
-            ]
-        ),
-
-        /// Protobuf definitions for the EdgeAgent service.
-        .target(
-            name: "EdgeAgentGRPC",
-            dependencies: [
-                .product(name: "GRPCCore", package: "grpc-swift"),
-                .product(name: "GRPCProtobuf", package: "grpc-swift-protobuf"),
-            ],
-            exclude: [
-                "Proto/edge_agent.protoset"
             ]
         ),
     ]
